28d295edcd5a5582cacd8c8c418febb72b15c22c
thvi611 <lovivivi611@gmail.com>
2022-12-27 22:50:14 +0700

    Final 3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PBL7-SongRegconition'`, () => {
+  it(`should have as title 'PBL6-SongRegconition'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PBL7-SongRegconition');
+    expect(app.title).toEqual('PBL6-SongRegconition');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PBL7-SongRegconition app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PBL6-SongRegconition app is running!');
   });
 });
